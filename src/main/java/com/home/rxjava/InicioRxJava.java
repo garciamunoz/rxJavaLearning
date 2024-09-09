6529cff2b23c55efe21784aa5a41cfd60769832e
Luis Garcia <garciamunozla@gmail.com>
2024-09-09 12:28:30 -0500

    Example about susbcription parameters

--- src/main/java/com/home/rxjava/InicioRxJava.java
+++ src/main/java/com/home/rxjava/InicioRxJava.java
@@ -4,9 +4,9 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class InicioRxJava {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         //Primer ejemplo de creacion de Observable
         Observable.just(1,2,3).
                 subscribe(System.out::println);
-    }
+    }*/
 }
---
+++ src/main/java/com/home/rxjava/ParametrosSubscripcion.java
@@ -0,0 +1,17 @@
+package com.home.rxjava;
+
+import io.reactivex.rxjava3.core.Observable;
+
+
+
+public class ParametrosSubscripcion {
+
+    public static void main(String[] args) {
+        //Ejemplo de susbscripcion con parámetros
+        //Declaración del observable
+        Observable.just("puto","rx","java")
+                .subscribe(item -> System.out.println(item),
+                           throwable -> System.out.println(throwable),
+                           () -> System.out.println("completado"));
+    }
+}
