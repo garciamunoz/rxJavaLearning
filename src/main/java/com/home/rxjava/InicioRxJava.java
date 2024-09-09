d9f501fb122c8850ebd5a860c581b5df0783b9bc
Luis Garcia <garciamunozla@gmail.com>
2024-09-09 12:15:02 -0500

    first commit

--- src/main/java/com/home/rxjava/InicioRxJava.java
+++ src/main/java/com/home/rxjava/InicioRxJava.java
@@ -5,7 +5,7 @@ import io.reactivex.rxjava3.core.Observable;
 public class InicioRxJava {
 
     public static void main(String[] args) {
-        //Primer ejemplo de creacion de Observables
+        //Primer ejemplo de creacion de Observable
         Observable.just(1,2,3).
                 subscribe(System.out::println);
     }
