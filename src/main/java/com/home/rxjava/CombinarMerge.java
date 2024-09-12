d61eecf5d0890b9b2b53fe65ab9ab9c7a4d2f311
Luis Garcia <garciamunozla@gmail.com>
2024-09-12 17:42:38 -0500

    Error management continue 2

---
+++ src/main/java/com/home/rxjava/CombinarMerge.java
@@ -0,0 +1,14 @@
+package com.home.rxjava;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class CombinarMerge {
+    /*public static void main(String[] args) {
+
+        Observable<Integer> obs1 = Observable.just(1,2,3);
+        Observable<Integer> obs2 = Observable.just(7,8,9);
+        Observable.merge(obs1,obs2).subscribe(
+            item -> System.out.println("El valor recibido es "+ item)
+        );
+    }*/
+}
---
+++ src/main/java/com/home/rxjava/CombinarZip.java
@@ -0,0 +1,4 @@
+package com.home.rxjava;
+
+public class CombinarZip {
+}
